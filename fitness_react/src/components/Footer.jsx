4d5b99758d36777f00d25b7b53ea30d7e95ec967
Peter67100 <abrankovic862@gmail.com>
2023-02-18 12:15:50 +0100

    kreiran footer

--- fitness_react/src/components/Footer.jsx
+++ fitness_react/src/components/Footer.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BsInstagram, BsFacebook, BsYoutube, BsTwitter} from 'react-icons/bs'
+import { Link } from 'react-router-dom';
 
 function Footer(){
-    return (
-    <></>
+return (
+<>
+<div className='footer'>
+<div className='logo'>
 
-    );
+</div>
+<div className='social'>
+<Link className='icon' to='http://instagram.com'><BsInstagram size={25}/></Link>
+<Link className='icon' to='http://facebook.com'><BsFacebook size={25}/></Link>
+<Link className='icon' to='http://twitter.com'><BsTwitter size={25}/></Link>
+<Link className='icon' to='http://youtube.com'><BsYoutube size={25}/></Link>
+</div>
+</div>
+</>
+
+);
 }
 
 export default Footer
