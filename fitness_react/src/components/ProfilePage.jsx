4fbbc9ec244abc446ee0997930ed7ffe7bde730f
teodora88 <nikolic8teodora@gmail.com>
2023-02-21 02:06:57 +0100

    sredjena profilna stranica

--- fitness_react/src/components/ProfilePage.jsx
+++ fitness_react/src/components/ProfilePage.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 function ProfilePage({currentUser}){
     return (
     <>
-        <div className='profile'>
-            <p className='name'>{currentUser.firstname}</p>
-            <p className='price'>Cena: {currentUser.lastname}</p>
-            <p className='description'>{currentUser.email}</p>
-            <p className='duration'>{currentUser.admin}</p>
-            <p className='type'>{currentUser.category.name}</p>
-            <p className='type'>{currentUser.birthday}</p>
+        <div className='profilePage'>
+            <p className='name'><b>Ime:</b> {currentUser.firstname}</p>
+            <p className='lastname'><b>Prezime:</b> {currentUser.lastname}</p>
+            <p className='email'><b>Email:</b> {currentUser.email}</p>
+            <p className='type'><b>Kategorija: </b>{currentUser.category.name}</p>
+            <p className='type'><b>Datum rodjenja:</b> {currentUser.birthday}</p>
         </div>
     </>
 
